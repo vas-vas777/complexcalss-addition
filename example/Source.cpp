ef6119bebdb01c8eda1c8629fdab9b90ccb38976
vas-vas777 <vasya1998@list.ru>
2017-05-18 00:10:23 +0300

    Update Source.cpp

--- example/Source.cpp
+++ example/Source.cpp
@@ -1,20 +1,26 @@
+#include"testclass.hpp"
 int main()
 {
-	double re = 5.0, im = 7.0;
-
+	Complex_t<double> a{ 5.0, 7.0 };
 	
-	Complex a(re, im);
+	Complex_t<double> b{ 5.0,7.0 };
 	
-	Complex b(re, im);
-
-
-
+	a == b; 
+	
+	a += a;
+	cout << "a+=a" << a << endl;
+	b -= {2, 3};
+	cout << "b-={2,3} " << b << endl;
+	a *= b;
+	a+b;
+	a*b;
+	a-b;
+	a/b;
 	cout << "a+b=" << a + b << endl;
 	cout << "a*b=" << a*b << endl;
 	cout << "a-b=" << a - b << endl;
 	cout << "a/b=" << a / b << endl;
 
-　
-	_getch();
+	system("pause");
 	return 0;
 }
